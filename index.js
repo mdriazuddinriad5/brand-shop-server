b4606c6d6ff34db620773f2a94cf0dd1c64ac5fc
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-10-19 12:33:45 +0600

    brand name and image posted in and read from database

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ app.use(express.json());
 app.use(cors());
 
 
-// mdriazuddinriad5
-// R1KZYPIk6HUsh0Yi
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikmixap.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -31,9 +27,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
+
+    const brandCollection = client.db("brandDB").collection("brands");
+
+    // post brand name and image into database
+
+    app.post('/brands', async (req, res) => {
+      const brands = req.body;
+      const result = await brandCollection.insertMany(brands);
+      res.send(result);
+    })
+
+    // read brand name and image from database
+
+    app.get('/brands', async (req, res) => {
+      const result = await brandCollection.find().toArray();
+      res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -45,9 +62,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Brand shop server is running...')
+  res.send('Brand shop server is running...')
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 })
